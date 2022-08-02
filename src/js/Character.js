d8d7306b2db21fb4efe8852c8f3965dfd13bc85f
Oleg Zasedatelev <ol.zasedatelew@yandex.ru>
2022-08-02 14:07:20 +0300

    second commit

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,7 +1,7 @@
 export default class Character {
-    constructor(name) {
-        this.name = name;
-        this.level = 1;
-        this.health = 100;
-    }
+  constructor(name) {
+    this.name = name;
+    this.level = 1;
+    this.health = 100;
+  }
 }
--- src/js/NewCharacter.js
+++ src/js/NewCharacter.js
@@ -1,32 +1,33 @@
-import Character from "./Character";
+import Character from './Character';
+/* eslint-disable no-underscore-dangle */
 
 export default class NewCharacter extends Character {
-    constructor(name) {
-        super(name);
-        this.stoned = false;
+  constructor(name) {
+    super(name);
+    this.stoned = false;
+  }
+
+  get stoned() {
+    return this._stoned;
+  }
+
+  set stoned(value) {
+    this._stoned = value;
+  }
+
+  get attack() {
+    if (this.distance < 0 || this.distance > 5) {
+      return 0;
     }
 
-    get stoned() {
-        return this._stoned;
+    let attack = this._attack - (this._attack / 10) * (this.distance - 1);
+    if (this.stoned === true) {
+      attack -= Math.log2(this.distance) * 5;
     }
+    return Math.floor(attack);
+  }
 
-    set stoned(value) {
-        this._stoned = value;
-    }
-
-    get attack() {
-        if (this.distance < 0 || this.distance > 5) {
-          return 0;
-        }
-    
-        let attack = this._attack - (this._attack / 10) * (this.distance - 1);
-        if (this.stoned === true) {
-          attack -= Math.log2(this.distance) * 5;
-        }
-        return Math.floor(attack);
-      }
-
-    set attack(value) {
-        this._attack = value;
-    }
+  set attack(value) {
+    this._attack = value;
+  }
 }
